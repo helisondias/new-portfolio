856da23b63f27b823f0a15943aa15563593ef642
Helison Dias <closedfps@gmail.com>
2024-03-29 15:05:32 -0300

    atualizações

--- react_portfolio/src/components/ProjectsContainer.jsx
+++ react_portfolio/src/components/ProjectsContainer.jsx
@@ -2,13 +2,74 @@ const ProjectsContainer = () => {
   return (
     <section className="projects-container">
       <h2>Projetos</h2>
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Suscipit esse
-        maiores quae unde nobis, ullam repellat est recusandae quaerat, minima
-        accusamus ducimus consequuntur at dolores deserunt officiis impedit,
-        earum delectus?
-      </p>
-      
+      <div className="projects-layout">
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Suscipit esse
+          maiores quae unde nobis, ullam repellat est recusandae quaerat, minima
+          accusamus ducimus consequuntur at dolores deserunt officiis impedit,
+          earum delectus?
+        </p>
+        <img
+          src="src\img\projeto-site.PNG"
+          alt="imagem do projeto filmografia"
+          className="projects-img"
+        />
+        <p className="projects-detail">
+          Meu primeiro projeto utilizando REACT, o projeto{" "}
+          <span>Filmografia</span> utiliza uma API de banco de dados do site TMDB
+          e busca informações sobre filmes. O site também conta com um sistema de
+          pesquisa que busca filmes especificos e trabalha com um sistema
+          multipáginas usando o ROUTE
+        </p>
+        <a href="https://filmografia-rho.vercel.app/" target="_blank" rel="noopener noreferrer">Ver projeto</a>
+        <br/>
+        <br/>
+        <img
+          src="src\img\projeto-site.PNG"
+          alt="imagem do projeto filmografia"
+          className="projects-img"
+        />
+        <p className="projects-detail">
+          Meu primeiro projeto utilizando REACT, o projeto{" "}
+          <span>Filmografia</span> utiliza uma API de banco de dados do site TMDB
+          e busca informações sobre filmes. O site também conta com um sistema de
+          pesquisa que busca filmes especificos e trabalha com um sistema
+          multipáginas usando o ROUTE
+        </p>
+        <a href="#" target="_blank" rel="noopener noreferrer">Ver projeto</a>
+        <br/>
+        <br/>
+        <img
+          src="src\img\projeto-site.PNG"
+          alt="imagem do projeto filmografia"
+          className="projects-img"
+        />
+        <p className="projects-detail">
+          Meu primeiro projeto utilizando REACT, o projeto{" "}
+          <span>Filmografia</span> utiliza uma API de banco de dados do site TMDB
+          e busca informações sobre filmes. O site também conta com um sistema de
+          pesquisa que busca filmes especificos e trabalha com um sistema
+          multipáginas usando o ROUTE
+        </p>
+
+        <a href="#" target="_blank" rel="noopener noreferrer">Ver projeto</a>
+        <br/>
+        <br/>
+        <img
+          src="src\img\projeto-site.PNG"
+          alt="imagem do projeto filmografia"
+          className="projects-img"
+        />
+        <p className="projects-detail">
+          Meu primeiro projeto utilizando REACT, o projeto{" "}
+          <span>Filmografia</span> utiliza uma API de banco de dados do site TMDB
+          e busca informações sobre filmes. O site também conta com um sistema de
+          pesquisa que busca filmes especificos e trabalha com um sistema
+          multipáginas usando o ROUTE
+        </p>
+
+        <a href="#" target="_blank" rel="noopener noreferrer">Ver projeto</a>
+      </div>
     </section>
   );
 };
